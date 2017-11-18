01121038bf6b5c48c20a99140a38a576d3632049
Jason Lu <syscallrax@gmail.com>
2017-11-18 00:50:57 -0600

    lib: Import string and printf

---
+++ lib/printf.c
@@ -0,0 +1,331 @@
+/*
+ * Copyright (C) 2017 Jason Lu
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <lib/printf.h>
+#include <lib/string.h>
+
+struct parameters {
+    char *buffer;
+    uint8_t base;
+    uint8_t uppercase;
+    bool is_negative;
+    int padding;
+    int zero;
+};
+
+struct printf_data {
+    char *buffer;
+    size_t max;
+    size_t used;
+    void (*putcf)(struct printf_data *, char);
+};
+
+static void snprintf_putcf(struct printf_data *data, char c)
+{
+    if (data->used < data->max)
+        data->buffer[data->used++] = c;
+}
+
+static void sprintf_putcf(struct printf_data *data, char c)
+{
+    data->buffer[data->used++] = c;
+}
+
+static void puts(struct parameters *params, struct printf_data *data,
+                 char *message)
+{
+    char *tmp = message;
+    int n = params->padding;
+    while (*tmp++ && n)
+        n--;
+    if (!params->zero) {
+        while (n--)
+            data->putcf(data, ' ');
+    }
+    if (params->zero) {
+        while (n--)
+            data->putcf(data, '0');
+    }
+    while (*message) {
+        data->putcf(data, *message++);
+    }
+}
+
+static void ull2a(unsigned long long num, struct parameters *params)
+{
+    char *buffer = params->buffer;
+    if (params->is_negative)
+        *buffer++ = '-';
+    int n = 0;
+    unsigned long long d = 1;
+    while (num / d >= params->base)
+        d *= params->base;
+    while (d != 0) {
+        int dgt = num / d;
+        num %= d;
+        d /= params->base;
+        if (n || dgt > 0 || d == 0) {
+            *buffer++ =
+                dgt + (dgt < 10 ? '0' : (params->uppercase ? 'A' : 'a') - 10);
+            ++n;
+        }
+    }
+    *buffer = 0;
+}
+
+static void ll2a(int num, struct parameters *p)
+{
+    if (num < 0) {
+        num = -num;
+        p->is_negative = true;
+    }
+    ull2a(num, p);
+}
+
+static void ul2a(unsigned long num, struct parameters *params)
+{
+    char *buffer = params->buffer;
+    if (params->is_negative)
+        *buffer++ = '-';
+    int n = 0;
+    unsigned long d = 1;
+    while (num / d >= params->base)
+        d *= params->base;
+    while (d != 0) {
+        int dgt = num / d;
+        num %= d;
+        d /= params->base;
+        if (n || dgt > 0 || d == 0) {
+            *buffer++ =
+                dgt + (dgt < 10 ? '0' : (params->uppercase ? 'A' : 'a') - 10);
+            ++n;
+        }
+    }
+    *buffer = 0;
+}
+
+static void l2a(int num, struct parameters *p)
+{
+    if (num < 0) {
+        num = -num;
+        p->is_negative = true;
+    }
+    ul2a(num, p);
+}
+
+static void ui2a(unsigned int num, struct parameters *params)
+{
+    char *buffer = params->buffer;
+    if (params->is_negative)
+        *buffer++ = '-';
+    int n = 0;
+    unsigned int d = 1;
+    while (num / d >= params->base)
+        d *= params->base;
+    while (d != 0) {
+        int dgt = num / d;
+        num %= d;
+        d /= params->base;
+        if (n || dgt > 0 || d == 0) {
+            *buffer++ =
+                dgt + (dgt < 10 ? '0' : (params->uppercase ? 'A' : 'a') - 10);
+            ++n;
+        }
+    }
+    *buffer = 0;
+}
+
+static void i2a(int num, struct parameters *p)
+{
+    if (num < 0) {
+        num = -num;
+        p->is_negative = true;
+    }
+    ui2a(num, p);
+}
+
+static int a2u(char *src)
+{
+    unsigned int num = 0;
+    int digit;
+    while ((digit = *src++ - '0') >= 0) {
+        if (digit > 10)
+            break;
+        num = num * 10 + digit;
+    }
+    return num;
+}
+
+static void printf_format(struct printf_data *data, const char *format,
+                          va_list arg)
+{
+    struct parameters params;
+    memset(&params, 0, sizeof(struct parameters));
+    const char *pos = format;
+    while (*pos != '\0') {
+        if (*pos == '%') {
+            // 22 chars for max value of long long in octal + 1 for null
+            char buffer[23];
+            params.buffer = buffer;
+            memset(buffer, 0, 23);
+            uint8_t lng = 0;
+            pos++;
+            if (*pos == '0') {
+                params.zero = 1;
+                pos++;
+            }
+            if (*pos >= '0' && *pos <= '9') {
+                int i = 0;
+                char padding[5];
+                while (*pos >= '0' && *pos <= '9') {
+                    padding[i++] = *pos++;
+                }
+                params.padding = a2u(padding);
+            }
+            if (*pos == 'l') {
+                lng++;
+                pos++;
+                if (*pos == 'l') {
+                    lng++;
+                    pos++;
+                }
+            }
+            switch (*pos++) {
+                case 'c':
+                    data->putcf(data, (char)va_arg(arg, int));
+                    break;
+                case 'd':
+                case 'i':
+                    params.base = 10;
+                    if (lng == 0)
+                        i2a(va_arg(arg, unsigned int), &params);
+                    else if (lng == 1)
+                        l2a(va_arg(arg, unsigned long), &params);
+                    else if (lng == 2)
+                        ll2a(va_arg(arg, unsigned long long), &params);
+                    puts(&params, data, params.buffer);
+                    break;
+                case 'u':
+                    params.base = 10;
+                    if (lng == 0)
+                        ui2a(va_arg(arg, unsigned int), &params);
+                    else if (lng == 1)
+                        ul2a(va_arg(arg, unsigned long), &params);
+                    else if (lng == 2)
+                        ull2a(va_arg(arg, unsigned long long), &params);
+                    puts(&params, data, params.buffer);
+                    break;
+                case 'o':
+                    params.base = 8;
+                    ui2a(va_arg(arg, unsigned int), &params);
+                    puts(&params, data, params.buffer);
+                    break;
+                case 's':
+                    puts(&params, data, va_arg(arg, char *));
+                    break;
+                case 'p':
+                    puts(&params, data, "0x");
+                    params.zero = 1;
+#if __SIZEOF_POINTER__ <= __SIZEOF_INT__
+                    params.padding = 8;
+                    lng = 0;
+#elif __SIZEOF_POINTER__ <= __SIZEOF_LONG__
+                    params.padding = 16;
+                    lng = 1;
+#elif __SIZEOF_POINTER__ <= __SIZEOF_LONG_LONG__
+                    params.padding = 16;
+                    lng = 2;
+#endif
+                case 'X':
+                    params.uppercase = 1;
+                case 'x':
+                    params.base = 16;
+                    if (lng == 0)
+                        ui2a(va_arg(arg, unsigned int), &params);
+                    else if (lng == 1)
+                        ul2a(va_arg(arg, unsigned long), &params);
+                    else if (lng == 2)
+                        ull2a(va_arg(arg, unsigned long long), &params);
+                    puts(&params, data, params.buffer);
+                    break;
+                case '%':
+                    data->putcf(data, '%');
+                    break;
+            }
+            memset(&params, 0, sizeof(struct parameters));
+        } else {
+            data->putcf(data, *pos++);
+        }
+    }
+}
+
+int vsprintf(char *s, const char *format, va_list arg)
+{
+    struct printf_data data = {
+        .buffer = s,
+        .putcf = sprintf_putcf,
+    };
+    printf_format(&data, format, arg);
+    s[data.used] = '\0';
+    return data.used;
+}
+
+int sprintf(char *s, const char *format, ...)
+{
+    va_list args;
+    int ret;
+    va_start(args, format);
+    ret = vsprintf(s, format, args);
+    va_end(args);
+    return ret;
+}
+
+int vsnprintf(char *s, size_t size, const char *format, va_list arg)
+{
+    struct printf_data data = {
+        .buffer = s,
+        .max = size,
+        .putcf = snprintf_putcf,
+    };
+    printf_format(&data, format, arg);
+    s[data.used] = '\0';
+    return data.used;
+}
+
+int snprintf(char *s, size_t size, const char *format, ...)
+{
+    va_list args;
+    int ret;
+    va_start(args, format);
+    ret = vsnprintf(s, size, format, args);
+    va_end(args);
+    return ret;
+}
---
+++ lib/string.c
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2017 Jason Lu
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <lib/string.h>
+
+void *memchr(const void *str, int c, size_t n)
+{
+    const uint8_t *d = str;
+    while (n--) {
+        if (*d == (uint8_t)c)
+            return (void *)d;
+        d++;
+    }
+    return NULL;
+}
+
+int memcmp(const void *str1, const void *str2, size_t n)
+{
+    const uint8_t *a = str1, *b = str2;
+    while (n--) {
+        if (*a != *b)
+            return *a - *b;
+        a++, b++;
+    }
+    return 0;
+}
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    uint8_t *a = dest;
+    const uint8_t *b = src;
+    while (n--) {
+        *a++ = *b++;
+    }
+    return dest;
+}
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+    if (dest < src)
+        return memcpy(dest, src, n);
+    else {
+        uint8_t *a = dest + n - 1;
+        const uint8_t *b = src + n - 1;
+        while (n--) {
+            *a-- = *b--;
+        }
+        return dest;
+    }
+}
+
+void *__attribute__((weak)) memset(void *str, int c, size_t n)
+{
+    uint8_t *a = str;
+    while (n--) {
+        *a++ = (uint8_t)c;
+    }
+    return str;
+}
+
+char *strcat(char *dest, const char *src)
+{
+    char *end = dest;
+    while (*end != '\0')
+        end++;
+    while (*src != '\0')
+        *end++ = *src++;
+    return dest;
+}
+
+char *strncat(char *dest, const char *src, size_t n)
+{
+    char *end = dest;
+    while (*end != '\0')
+        end++;
+    while (n--) {
+        if (!(*end++ = *src++))
+            return dest;
+    }
+    *end = '\0';
+    return dest;
+}
+
+char *strchr(const char *str, int c)
+{
+    // The C standard is weird
+    char *ret = (char *)str;
+    while (*ret != (char)c) {
+        if (*ret++ == '\0')
+            return NULL;
+    }
+    return ret;
+}
+
+int strcmp(const char *str1, const char *str2)
+{
+    while (*str1 && (*str1 == *str2))
+        str1++, str2++;
+    return *str1 - *str2;
+}
+
+int strncmp(const char *str1, const char *str2, size_t n)
+{
+    while (n-- && *str1) {
+        if (*str1 != *str2)
+            return *str1 - *str2;
+        str1++, str2++;
+    }
+    return 0;
+}
+
+char *strcpy(char *dest, const char *src)
+{
+    for (; *src != '\0'; dest++, src++)
+        *dest = *src;
+    return dest;
+}
+
+char *strncpy(char *dest, const char *src, size_t n)
+{
+    while ((n-- > 0) && *src) {
+        *dest++ = *src++;
+    }
+    while (n-- > 0)
+        *dest = '\0';
+    return dest;
+}
+
+size_t strlen(const char *str)
+{
+    size_t n = 0;
+    while (*str++ != '\0')
+        n++;
+    return n;
+}
+
+char *strrchr(const char *str, int c)
+{
+    char *last = NULL;
+    while (*str++ != '\0') {
+        if (*str == c)
+            last = (char *)str;
+    }
+    return last;
+}
+
+char *strstr(const char *haystack, const char *needle)
+{
+    size_t n = strlen(needle);
+    while (*haystack)
+        if (!memcmp(haystack++, needle, n))
+            return (char *)haystack - 1;
+    return 0;
+}
+
+size_t strcspn(const char *s1, const char *s2)
+{
+    const char *sc1;
+    for (sc1 = s1; *sc1 != '\0'; sc1++)
+        if (strchr(s2, *sc1) != NULL)
+            return (sc1 - s1);
+    return sc1 - s1; /* terminating nulls match */
+}
+
+size_t strspn(const char *s1, const char *s2)
+{
+    const char *sc1;
+    for (sc1 = s1; *sc1 != '\0'; sc1++)
+        if (strchr(s2, *sc1) == NULL)
+            return (sc1 - s1);
+    return sc1 - s1; /* terminating nulls don't match */
+}
+
+char *strtok_r(char *s, const char *delimiters, char **lasts)
+{
+    char *sbegin, *send;
+    sbegin = s ? s : *lasts;
+    sbegin += strspn(sbegin, delimiters);
+
+    if (*sbegin == '\0') {
+        *lasts = (char *)"";
+        return NULL;
+    }
+
+    send = sbegin + strcspn(sbegin, delimiters);
+
+    if (*send != '\0')
+        *send++ = '\0';
+
+    *lasts = send;
+
+    return sbegin;
+}

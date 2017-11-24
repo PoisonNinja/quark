3a97af25666eea5ed27bbad8e11322f9bba24687
Jason Lu <syscallrax@gmail.com>
2017-11-24 17:33:54 -0600

    Add panic code

--- include/kernel.h
+++ include/kernel.h
@@ -2,5 +2,10 @@
 
 #include <kernel/log.h>
 
+namespace Kernel
+{
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
+
+void __attribute__((noreturn)) panic(const char* message, ...);
+}
---
+++ kernel/panic.cc
@@ -0,0 +1,24 @@
+#include <kernel.h>
+#include <lib/printf.h>
+#include <lib/string.h>
+#include <stdarg.h>
+
+#define PANIC_MAX 1024
+
+static char panic_buffer[PANIC_MAX];
+
+namespace Kernel
+{
+void __attribute__((noreturn)) panic(const char* format, ...)
+{
+    size_t r = 0;
+    memset(panic_buffer, 0, PANIC_MAX);
+    va_list args;
+    va_start(args, format);
+    r = vsnprintf(panic_buffer, PANIC_MAX, format, args);
+    va_end(args);
+    Log::printk(Log::ERROR, "%s", panic_buffer);
+    for (;;)
+        asm("hlt");
+}
+}

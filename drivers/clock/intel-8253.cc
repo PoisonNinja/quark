9ca417d1b6c6046ff8331b3693ac4bf2a641e179
Jason Lu <syscallrax@gmail.com>
2017-12-28 23:04:46 -0600

    drivers: intel-8253: Initial implementation
    Implements the Timer class and provides both scheduled interrupts and periodic
    ticks. It's currently used as the system timer if installed, which it is
    for x86 based systems.

--- drivers/clock/intel-8253.cc
+++ drivers/clock/intel-8253.cc
@@ -1,25 +1,50 @@
 #include <arch/drivers/io.h>
+#include <cpu/interrupt.h>
 #include <drivers/clock/intel-8253.h>
+#include <kernel/time/time.h>
 
 namespace Time
 {
-#define FREQUENCY 1000  // in HZ
+const static int FREQUENCY = 1193181;  // Frequency in HZ
+const static int HZ = 1000;            // # of interrupts per second
+const static int CHANNEL0 = 0x40;
+const static int CHANNEL1 = 0x41;
+const static int CHANNEL2 = 0x42;
+const static int COMMAND = 0x43;
+const static time_t PRECISION = 1000;  // 1000 nanoseconds
 
-time_t Intel8253::read()
+status_t Intel8253::schedule(time_t interval)
 {
-    return counter;
+    outb(COMMAND, 0x38);  // Channel 0, lobyte/hibyte, mode 4, 16-bit
+    outb(CHANNEL0, static_cast<uint8_t>(interval & 0xFF));
+    outb(CHANNEL0, static_cast<uint8_t>((interval >> 8) & 0xFF));
+    return SUCCESS;
 }
 
-time_t Intel8253::frequency()
+status_t Intel8253::periodic()
 {
-    return FREQUENCY;
-}
-
-status_t Intel8253::enable()
-{
+    uint16_t divisor = FREQUENCY / HZ;
+    outb(COMMAND, 0x36);  // Channel 0, lobyte/hibyte, mode 2, 16-bit
+    outb(CHANNEL0, static_cast<uint8_t>(divisor & 0xFF));
+    outb(CHANNEL0, static_cast<uint8_t>((divisor >> 8) & 0xFF));
+    return SUCCESS;
 }
 
 status_t Intel8253::disable()
 {
+    outb(COMMAND, 0x30);
+    outb(CHANNEL0, 0);
+    outb(CHANNEL0, 0);
+    return SUCCESS;
+}
+
+time_t Intel8253::precision()
+{
+    return PRECISION;
+}
+
+int Intel8253::irq()
+{
+    return Interrupt::irq_to_interrupt(0);
 }
 }  // namespace Time
--- include/drivers/clock/intel-8253.h
+++ include/drivers/clock/intel-8253.h
@@ -2,17 +2,17 @@
 
 #include <kernel/time/clock.h>
 
+struct interrupt_ctx;
+
 namespace Time
 {
-class Intel8253 : public Clock
+class Intel8253 : public Timer
 {
 public:
-    time_t read() override;
-    time_t frequency() override;
-    status_t enable() override;
+    status_t schedule(time_t interval) override;
+    status_t periodic() override;
     status_t disable() override;
-
-private:
-    time_t counter;
+    time_t precision() override;
+    int irq() override;
 };
 }  // namespace Time

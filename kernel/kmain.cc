76711b578f51887262674a7018108657a5e7daf7
Jason Lu <syscallrax@gmail.com>
2018-01-31 00:37:19 -0600

    Remove test code

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -5,14 +5,11 @@
 #include <kernel/version.h>
 #include <mm/mm.h>
 
-#include <mm/virtual.h>
-
 void kmain(struct Boot::info& info)
 {
     Log::printk(Log::INFO, "%s\n", OS_STRING);
     Log::printk(Log::INFO, "Command line: %s\n", info.cmdline);
     Memory::init(info);
-    Memory::Virtual::clone();
     Interrupt::init();
     Time::init();
     Interrupt::enable();

8e910838b58c11d7cb654d45b761d00c0eb8a4d3
Jason Lu <syscallrax@gmail.com>
2018-02-20 22:21:54 -0600

    fs: Initialize kernel_process to root

--- fs/fs.cc
+++ fs/fs.cc
@@ -3,6 +3,7 @@
 #include <fs/initfs/initfs.h>
 #include <fs/inode.h>
 #include <fs/vnode.h>
+#include <proc/sched.h>
 
 namespace Filesystem
 {
@@ -11,6 +12,7 @@ void init()
     Ref<Inode> iroot(new InitFS::Directory(0, 0, 0755));
     Ref<Vnode> vroot(new Vnode(iroot));
     Ref<Descriptor> droot(new Descriptor(vroot));
-    droot->open("/test", 0, 0);
+    Scheduler::get_current_process()->cwd = droot;
+    Scheduler::get_current_process()->root = droot;
 }
 }

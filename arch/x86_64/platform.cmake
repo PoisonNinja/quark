17ed345b3246717d91d15ac41f7375d4e6c3bcaa
Jason Lu <syscallrax@gmail.com>
2018-02-24 23:48:15 -0600

    Update compilation flags
    Remove Clang specific flags (such as target and color-diagnostics)
    
    GCC also has libgcc which contains C++ freestanding headers, so we won't add
    -nostdinc++. Most stdlib stuff should be disabled with -ffreestanding anyways.
    
    We also need -zmax-page-size for GCC or else Multiboot can't find the header

--- arch/x86_64/platform.cmake
+++ arch/x86_64/platform.cmake
@@ -3,9 +3,9 @@ add_definitions(
     -DX86_64
 )
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --target=x86_64-pc-none-elf -mcmodel=kernel -mno-red-zone")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcmodel=kernel -mno-red-zone -zmax-page-size=0x1000 -T${CMAKE_SOURCE_DIR}/arch/x86_64/quark.lds")
 
 # Be careful of what you pass in here. Input will go directly to LLD, so no Clang flags allowed
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T${CMAKE_SOURCE_DIR}/arch/x86_64/quark.lds")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ")
 
 set(CMAKE_ASM_NASM_COMPILE_OBJECT "${CMAKE_ASM_NASM_COMPILE_OBJECT} -felf64")

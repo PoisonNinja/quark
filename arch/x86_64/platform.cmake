d66b4871cf1392980128c841ade7027d925b8052
Jason Lu <syscallrax@gmail.com>
2018-02-25 23:49:30 -0600

    x86_64: Move linker flags to CMAKE_EXE_LINKER_FLAGS

--- arch/x86_64/platform.cmake
+++ arch/x86_64/platform.cmake
@@ -3,9 +3,8 @@ add_definitions(
     -DX86_64
 )
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcmodel=kernel -mno-red-zone -zmax-page-size=0x1000 -T${CMAKE_SOURCE_DIR}/arch/x86_64/quark.lds")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcmodel=kernel -mno-red-zone")
 
-# Be careful of what you pass in here. Input will go directly to LLD, so no Clang flags allowed
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -zmax-page-size=0x1000 -T${CMAKE_SOURCE_DIR}/arch/x86_64/quark.lds")
 
 set(CMAKE_ASM_NASM_COMPILE_OBJECT "${CMAKE_ASM_NASM_COMPILE_OBJECT} -felf64")

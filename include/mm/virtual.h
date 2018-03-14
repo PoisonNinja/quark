cff9c4184643cc79cd8d40bf0d6d0d163cface72
Jason Lu <syscallrax@gmail.com>
2018-03-13 22:00:50 -0500

    mm: Remove PAGE_PRESENT

--- include/mm/virtual.h
+++ include/mm/virtual.h
@@ -7,13 +7,12 @@ namespace Memory
 {
 namespace Virtual
 {
-#define PAGE_PRESENT 0x1
-#define PAGE_WRITABLE 0x2
-#define PAGE_USER 0x4
-#define PAGE_GLOBAL 0x8
-#define PAGE_NX 0x10
-#define PAGE_HUGE 0x20
-#define PAGE_COW 0x40
+#define PAGE_WRITABLE 0x1
+#define PAGE_USER 0x2
+#define PAGE_GLOBAL 0x4
+#define PAGE_NX 0x8
+#define PAGE_HUGE 0x10
+#define PAGE_COW 0x20
 
 bool map(addr_t v, addr_t p, int flags);
 bool map(addr_t v, addr_t p, size_t size, int flags);

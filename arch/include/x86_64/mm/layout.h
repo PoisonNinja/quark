a0b34762471ec49b7412691e91fc879c4271894f
Jason Lu <syscallrax@gmail.com>
2018-03-17 00:09:53 -0400

    x86_64: Define USER_{START,END}

--- arch/include/x86_64/mm/layout.h
+++ arch/include/x86_64/mm/layout.h
@@ -1,3 +1,8 @@
 #pragma once
 
-#define HEAP_START 0xFFFF880000000000
+#include <types.h>
+
+const addr_t HEAP_START = 0xFFFF880000000000;
+
+const addr_t USER_START = 0x400000;
+const addr_t USER_END = 0x7FFFFFFFFFFF;

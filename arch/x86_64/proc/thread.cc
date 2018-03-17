a20bb8fe17dabf1ec6577607c3b7d547b4d9cc29
Jason Lu <syscallrax@gmail.com>
2018-03-17 00:14:03 -0400

    x86_64: Set thread argv, envp, and stack dynamically

--- arch/x86_64/proc/thread.cc
+++ arch/x86_64/proc/thread.cc
@@ -1,5 +1,7 @@
 #include <arch/cpu/gdt.h>
+#include <arch/mm/layout.h>
 #include <cpu/interrupt.h>
+#include <kernel.h>
 #include <lib/string.h>
 #include <mm/physical.h>
 #include <mm/virtual.h>
@@ -62,24 +64,78 @@ status_t Thread::load_context(struct interrupt_ctx* ctx)
     return SUCCESS;
 }
 
-void Thread::load(addr_t entry, int argc, const char* argv[], int envc,
+bool Thread::load(addr_t entry, int argc, const char* argv[], int envc,
                   const char* envp[])
 {
-    addr_t phys_stack = Memory::Physical::allocate();
-    Memory::Virtual::map(0x1000, phys_stack, PAGE_WRITABLE | PAGE_USER);
-    String::memset((void*)0x1000, 0, 4096);
+    size_t argv_size = sizeof(char*) * (argc + 1);  // argv is null terminated
+    size_t envp_size = sizeof(char*) * (envc + 1);  // envp is null terminated
+    for (int i = 0; i < argc; i++) {
+        argv_size += String::strlen(argv[i]) + 1;
+    };
+    for (int i = 0; i < envc; i++) {
+        envp_size += String::strlen(envp[i]) + 1;
+    }
+    addr_t argv_zone;
+    addr_t envp_zone;
+    addr_t stack_zone;
+    if (sections->locate_range(argv_zone, USER_START, argv_size)) {
+        Log::printk(Log::DEBUG, "argv located at %p\n", argv_zone);
+        sections->add_section(argv_zone, argv_size);
+    } else {
+        Log::printk(Log::ERROR, "Failed to locate argv\n");
+        return false;
+    }
+    if (sections->locate_range(envp_zone, USER_START, envp_size)) {
+        Log::printk(Log::DEBUG, "envp located at %p\n", envp_zone);
+        sections->add_section(envp_zone, envp_size);
+    } else {
+        Log::printk(Log::ERROR, "Failed to locate envp\n");
+        return false;
+    }
+    if (sections->locate_range(stack_zone, USER_START, 0x1000)) {
+        Log::printk(Log::DEBUG, "Stack located at %p\n", stack_zone);
+        sections->add_section(stack_zone, 0x1000);
+    } else {
+        Log::printk(Log::ERROR, "Failed to locate stack\n");
+        return false;
+    }
+    Memory::Virtual::map(envp_zone, Memory::Physical::allocate(),
+                         PAGE_USER | PAGE_NX | PAGE_WRITABLE);
+    Memory::Virtual::map(argv_zone, Memory::Physical::allocate(),
+                         PAGE_USER | PAGE_NX | PAGE_WRITABLE);
+    Memory::Virtual::map(stack_zone, Memory::Physical::allocate(),
+                         PAGE_WRITABLE | PAGE_USER);
+    char* target =
+        reinterpret_cast<char*>(argv_zone + (sizeof(char*) * (argc + 1)));
+    char** target_argv = reinterpret_cast<char**>(argv_zone);
+    for (int i = 0; i < argc; i++) {
+        String::strcpy(target, argv[i]);
+        target_argv[i] = target;
+        target += String::strlen(argv[i]) + 1;
+    }
+    target_argv[argc] = 0;
+    target = reinterpret_cast<char*>(envp_zone + (sizeof(char*) * (envc + 1)));
+    char** target_envp = reinterpret_cast<char**>(envp_zone);
+    for (int i = 0; i < envc; i++) {
+        String::strcpy(target, envp[i]);
+        target_envp[i] = target;
+        target += String::strlen(envp[i]) + 1;
+    }
+    target_envp[envc] = 0;
+    String::memset((void*)stack_zone, 0, 4096);
     String::memset(&cpu_ctx, 0, sizeof(cpu_ctx));
     cpu_ctx.rip = entry;
     cpu_ctx.rdi = argc;
-    cpu_ctx.rsi = reinterpret_cast<uint64_t>(argv);
+    cpu_ctx.rsi = reinterpret_cast<uint64_t>(target_argv);
     cpu_ctx.rdx = envc;
-    cpu_ctx.rcx = reinterpret_cast<uint64_t>(envp);
+    cpu_ctx.rcx = reinterpret_cast<uint64_t>(target_envp);
     cpu_ctx.cs = 0x18 | 3;
     cpu_ctx.ds = 0x20 | 3;
     cpu_ctx.ss = 0x20 | 3;
-    cpu_ctx.rsp = cpu_ctx.rbp = 0x2000;
+    cpu_ctx.rsp = cpu_ctx.rbp = reinterpret_cast<uint64_t>(stack_zone);
     cpu_ctx.rflags = 0x200;
     kernel_stack = reinterpret_cast<addr_t>(new uint8_t[0x1000]) + 0x1000;
+    return true;
 }
 
 void arch_set_stack(addr_t stack)

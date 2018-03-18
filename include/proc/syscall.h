6bc460062645f4ed9e71563f141b858b1baac07c
Jason Lu <syscallrax@gmail.com>
2018-03-17 23:39:40 -0400

    syscall: Implement open and close

--- include/proc/syscall.h
+++ include/proc/syscall.h
@@ -2,7 +2,11 @@
 
 namespace Syscall
 {
-const int SYS_exit = 60;
+#define SYS_read 0
+#define SYS_write 1
+#define SYS_open 2
+#define SYS_close 3
+#define SYS_exit 60
 
 void init();
 }
--- proc/syscall.cc
+++ proc/syscall.cc
@@ -6,8 +6,37 @@
 
 namespace Syscall
 {
+static int sys_open(const char* path, int flags, mode_t mode)
+{
+    Log::printk(Log::DEBUG, "[sys_open] = %s, %X, %X\n", path, flags, mode);
+    Ref<Filesystem::Descriptor> start(nullptr);
+    if (*path == '/') {
+        start = Scheduler::get_current_process()->root;
+    } else {
+        start = start = Scheduler::get_current_process()->cwd;
+    }
+    Ref<Filesystem::Descriptor> file = start->open(path, flags, mode);
+    int ret = Scheduler::get_current_process()->fds.add(file);
+    if (Scheduler::get_current_process()->fds[ret] != file) {
+        Log::printk(Log::ERROR,
+                    "WTF, someone is lying about the file descriptor...\n");
+        return -1;
+    }
+    return ret;
+}
+
+static int sys_close(int fd)
+{
+    if (!Scheduler::get_current_process()->fds.remove(fd)) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
 static void sys_exit(int val)
 {
+    Log::printk(Log::DEBUG, "[sys_exit] = %d\n", val);
     Scheduler::remove(Scheduler::get_current_thread());
     for (;;) {
         __asm__("hlt");
@@ -18,7 +47,9 @@ static void* syscall_table[256];
 
 static void handler(int, void*, struct interrupt_ctx* ctx)
 {
-    Log::printk(Log::DEBUG, "Received system call %d\n", ctx->rax);
+    Log::printk(Log::DEBUG,
+                "Received system call %d, %llX %llX %llX %llX %llX\n", ctx->rax,
+                ctx->rdi, ctx->rsi, ctx->rdx, ctx->rcx, ctx->r8);
     if (!syscall_table[ctx->rax]) {
         Log::printk(Log::ERROR, "Received invalid syscall #%d\n", ctx->rax);
         ctx->rax = -ENOSYS;
@@ -35,6 +66,8 @@ static struct Interrupt::Handler handler_data(handler, "syscall",
 
 void init()
 {
+    syscall_table[SYS_open] = reinterpret_cast<void*>(sys_open);
+    syscall_table[SYS_close] = reinterpret_cast<void*>(sys_close);
     syscall_table[SYS_exit] = reinterpret_cast<void*>(sys_exit);
     Interrupt::register_handler(0x80, handler_data);
 }

b4208ac6be9c02e61ec2669a2010885a965296a7
Jason Lu <syscallrax@gmail.com>
2018-04-14 17:21:32 -0500

    lib: Allow overriding memcpy
    memcpy is one of the most frequently called functions in the kernel, and it
    currently is a dumb one byte loop that has significant performance penalties.
    
    To allow architectures to override it if they wish to, mark it as weak.

--- lib/string.cc
+++ lib/string.cc
@@ -44,11 +44,12 @@ void *memchr(const void *str, int c, size_t n)
     return nullptr;
 }
 
-void *memrchr(const void *str, int c, size_t n) {
-    const uint8_t *d = (uint8_t*)str + n;
+void *memrchr(const void *str, int c, size_t n)
+{
+    const uint8_t *d = (uint8_t *)str + n;
     while (n--) {
         if (*--d == (uint8_t)c)
-            return (void*)d;
+            return (void *)d;
     }
     return nullptr;
 }
@@ -64,7 +65,7 @@ int memcmp(const void *str1, const void *str2, size_t n)
     return 0;
 }
 
-void *memcpy(void *dest, const void *src, size_t n)
+void *__attribute__((weak)) memcpy(void *dest, const void *src, size_t n)
 {
     uint8_t *a = (uint8_t *)dest;
     const uint8_t *b = (uint8_t *)src;

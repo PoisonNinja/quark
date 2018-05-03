4e76e6265b508f18d208737bfebe5c8e1f25b6f2
Jason Lu <syscallrax@gmail.com>
2018-05-03 01:29:48 -0500

    i686: Place heap start in slot 1021

--- arch/include/i686/mm/layout.h
+++ arch/include/i686/mm/layout.h
@@ -2,9 +2,9 @@
 
 #include <types.h>
 
-constexpr addr_t HEAP_START = 0xFFFF880000000000;
+constexpr addr_t HEAP_START = 0xFF400000;  // Slot 1021
 
-constexpr addr_t STACK_START = 0xFF800000;
+constexpr addr_t STACK_START = 0xFF800000;  // Slot 1022
 
 constexpr addr_t USER_START = 0x400000;
 constexpr addr_t USER_END = 0x7FFFFFFFFFFF;

f151db0a3b9c671a065d05774dbe8ae9783b60aa
Jason Lu <syscallrax@gmail.com>
2018-05-04 12:10:46 -0500

    i686: Update MSR macros

--- arch/include/i686/cpu/cpu.h
+++ arch/include/i686/cpu/cpu.h
@@ -6,8 +6,11 @@ namespace CPU
 {
 namespace X86
 {
-constexpr uint64_t msr_efer = 0xC0000080;
+constexpr uint64_t msr_sysenter_cs = 0x174;
+constexpr uint64_t msr_sysenter_esp = 0x175;
+constexpr uint64_t msr_sysenter_eip = 0x176;
 
+constexpr uint64_t msr_efer = 0xC0000080;
 constexpr uint64_t msr_star = 0xC0000081;
 constexpr uint64_t msr_lstar = 0xC0000082;
 constexpr uint64_t msr_cstar = 0xC0000083;
@@ -15,18 +18,16 @@ constexpr uint64_t msr_fmask = 0xC0000084;
 
 constexpr uint64_t msr_kernel_gs_base = 0xC0000102;
 
-static inline void wrmsr(uint64_t msr, uint64_t value)
+static inline void wrmsr(uint32_t msr_id, uint64_t msr_value)
 {
-    uint32_t low = value & 0xFFFFFFFF;
-    uint32_t high = value >> 32;
-    asm volatile("wrmsr" : : "c"(msr), "a"(low), "d"(high));
+    asm volatile("wrmsr" : : "c"(msr_id), "A"(msr_value));
 }
 
-static inline uint64_t rdmsr(uint64_t msr)
+static inline uint64_t rdmsr(uint32_t msr_id)
 {
-    uint32_t low, high;
-    asm volatile("rdmsr" : "=a"(low), "=d"(high) : "c"(msr));
-    return ((uint64_t)high << 32) | low;
+    uint64_t msr_value;
+    asm volatile("rdmsr" : "=A"(msr_value) : "c"(msr_id));
+    return msr_value;
 }
 
 void init();

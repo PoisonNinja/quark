c3da35ac76b11642d841e428dc7c7269f0e87449
Jason Lu <syscallrax@gmail.com>
2018-05-04 16:12:49 -0500

    i686: mm: Implement test

--- arch/i686/mm/test.cc
+++ arch/i686/mm/test.cc
@@ -9,7 +9,14 @@ namespace Virtual
 {
 bool test(addr_t v)
 {
-    return false;
+    struct page_table* pd = (struct page_table*)Memory::X86::decode_fractal(
+        Memory::X86::recursive_entry, Memory::X86::recursive_entry);
+    struct page_table* pt = (struct page_table*)Memory::X86::decode_fractal(
+        Memory::X86::recursive_entry, Memory::X86::pd_index(v));
+    if (!pd->pages[Memory::X86::pd_index(v)].present ||
+        !pt->pages[Memory::X86::pt_index(v)].present)
+        return false;
+    return true;
 }
 }  // namespace Virtual
 }  // namespace Memory

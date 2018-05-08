b1418ecb2b43cf3c64d6ab14e951261962ffcbc4
Jason Lu <syscallrax@gmail.com>
2018-05-07 23:04:45 -0500

    i686: Update comments

--- arch/i686/mm/fork.cc
+++ arch/i686/mm/fork.cc
@@ -40,54 +40,57 @@ void __copy_pt_entry(struct page_table* new_pt, struct page_table* old_pt,
 
 addr_t fork()
 {
-    // Fractal mapping address of original PML4
+    // Fractal mapping address of original page directory
     struct page_table* old_pd = (struct page_table*)Memory::X86::decode_fractal(
         Memory::X86::recursive_entry, Memory::X86::recursive_entry);
     /*
-     * Fractal mapping address of original PML4
-     * Notice that the first parameter is Memory::X64::copy_entry, while the
-     * rest are Memory::X64::recursive_entry. This is because while the address
-     * of the new PML4 is loaded into the Memory::X64::copy_entry slot of the
-     * current PML4, the new PML4 recursive mapping is still
-     * Memory::X64::recursive_entry in itself.
+     * Fractal mapping address of original page directory
+     * Notice that the first parameter is Memory::X86::copy_entry, while the
+     * other is Memory::X86::recursive_entry? This is because while the address
+     * of the new page directory is loaded into the Memory::X86::copy_entry slot
+     * of the current page directory, the new page directory recursive mapping
+     * is still Memory::X86::recursive_entry in itself.
      */
     struct page_table* new_pd = (struct page_table*)Memory::X86::decode_fractal(
         Memory::X86::copy_entry, Memory::X86::recursive_entry);
-    // Allocate a new page for the PML4
+    // Allocate a new page for the page directory
     addr_t fork_pd_phys = Memory::Physical::allocate();
 
     /*
-     * Map the new PML4 into a page that we control, which we reserved
+     * Map the new page directory into a page that we control, which we reserved
      * using fork_page. This is so we can set up the fractal mapping
-     * for the new PML4.
+     * for the new page directory.
      */
     Memory::Virtual::map(reinterpret_cast<addr_t>(fork_page_pointer),
                          fork_pd_phys, PAGE_WRITABLE);
     /*
-     * Copy the entire PML4. However, only the kernel pages will remain
-     * (e.g. pml4_index(x) >= 256), as we want to duplicate the user pages
+     * Copy the entire page directory. However, only the kernel pages will
+     * remain (e.g. pd_index(x) >= 768 (3 GB)), as we want to duplicate the user
+     * pages. Essentially, the kernel pages will be linked together so any
+     * changes in kernel space (such as malloc) will propogate to kernel space
+     * in other threads
      */
     String::memcpy(fork_page_pointer, old_pd, sizeof(struct page_table));
 
-    // Set up fractal mapping for the new PML4
+    // Set up fractal mapping for the new page directory
     fork_page_pointer->pages[Memory::X86::recursive_entry].address =
         fork_pd_phys / 0x1000;
 
-    // Load the new PML4 into the Memory::X64::copy_entry slot of the current
-    // PML4
+    // Load the new page directory into the Memory::X86::copy_entry slot of the
+    // current page directory
     old_pd->pages[Memory::X86::copy_entry].present = 1;
     old_pd->pages[Memory::X86::copy_entry].writable = 1;
     old_pd->pages[Memory::X86::copy_entry].address = fork_pd_phys / 0x1000;
 
     Memory::X86::invlpg(reinterpret_cast<addr_t>(new_pd));
 
-    // Copy only user pages
+    // Copy only user pages (0 - 3GB)
     for (int i = 0; i < 768; i++) {
         if (old_pd->pages[i].present) {
             // Copy the page data
             String::memcpy(&new_pd->pages[i], &old_pd->pages[i],
                            sizeof(struct page));
-            // Allocate a new PDPT
+            // Allocate a new page table
             addr_t new_pt = Memory::Physical::allocate();
             // Set the page address
             new_pd->pages[i].address = new_pt / 0x1000;

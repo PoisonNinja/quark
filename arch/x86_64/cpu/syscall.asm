79da001d0de13e5281d54f6757bc2aa88830d7ab
Jason Lu <syscallrax@gmail.com>
2018-06-04 09:38:46 -0500

    x86_64: syscall: Simulate an interrupt frame
    Before, one of the issues was that system calls could not modify
    the system call return state. While most system calls did not
    need this, some calls such as signal handlers and execve did.
    
    Therefore, the syscall handler now builds an interrupt frame
    exactly like an interrupt handler, passes it in to the system
    call handler so system calls can access and modify it, and
    then restores the state.

--- arch/x86_64/cpu/syscall.asm
+++ arch/x86_64/cpu/syscall.asm
@@ -1,54 +1,71 @@
 
-extern syscall_table
+extern syscall_trampoline
 
 global syscall_sysret_wrapper
 syscall_sysret_wrapper:
     swapgs              ; Thread struct is saved in KernelGSBase, swap it into GS
     mov [gs:88], rsp    ; Save user RSP into thread->cpu_ctx.rsp
-    cmp rax, 57         ; Fork needs special handling, but this is expensive
-                        ; so try to avoid doing this
-    jne continue        ; Not fork, so we can skip these steps
-    mov [gs:8], r15     ; Load these registers into thread state so we can copy
-    mov [gs:16], r14
-    mov [gs:24], r13
-    mov [gs:32], r12
-    mov [gs:96], rbp
-    mov [gs:120], rbx
-    mov [gs:136], rcx   ; RIP
-continue:
     mov rsp, [gs:176]   ; Load kernel RSP from thread->kernel_stack.
-    push qword [gs:88]  ; Push the user RSP onto the stack
+
+    ; Build a simulated interrupt frame
+    push qword 0x1B ; SS
+    push qword [gs:88] ; User RSP
+    push r11  ; RFLAGS
+    push qword 0x23 ; CS
+    push rcx  ; RIP
+
+    push qword 0 ; Error code
+    push qword 0 ; Interrupt number
+
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rbp
+    push rdi
+    push rsi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    push qword 0x1B ; DS
+
     swapgs              ; Swap back GS values. This is important because
                         ; we may not reach the end of this function, especially
                         ; if the system call is sys_exit. If that happens,
                         ; future syscalls will panic because GS contains
                         ; the wrong value
-    push rdi            ; Preserve values
-    push rsi
-    push rdx
-    push rcx
-    push r8
-    push r9
-    push r10
-    push r11
-    mov rcx, r10
-    cmp rax, 256                           ; Check if syscall # is valid index
-                                           ; TODO: Get 256 from header file
-    jge invalid_syscall
-    cmp qword [syscall_table + rax * 8], 0 ; Check if syscall exists
-    je invalid_syscall
-    call [syscall_table + rax * 8] ; Call the system call
-return:
-    pop r11             ; Restore the registers
+
+    mov rdi, rsp
+    call syscall_trampoline
+
+    pop r15 ; DS
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rax ; Skip R11 (RFLAGS)
     pop r10
     pop r9
     pop r8
-    pop rcx
-    pop rdx
     pop rsi
     pop rdi
-    pop rsp             ; Pop user stack back into RSP
+    pop rbp
+    pop rdx
+    pop rax ; Skip RCX (Return RIP)
+    pop rbx
+    pop rax
+
+    add rsp, 16 ; Skip the rest of the IRET frame
+    pop rcx     ; RIP
+    add rsp, 8
+    pop r11     ; RFLAGS
+    pop rsp
+
     o64 sysret          ; Return
-invalid_syscall:
-    mov rax, -38        ; -ENOSYS
-    jmp return
---
+++ arch/x86_64/cpu/trampoline.cc
@@ -0,0 +1,27 @@
+#include <arch/cpu/registers.h>
+#include <errno.h>
+#include <kernel.h>
+#include <proc/sched.h>
+#include <proc/thread.h>
+
+extern void* syscall_table[];
+
+extern "C" void syscall_trampoline(struct InterruptContext* ctx)
+{
+    save_context(ctx, &Scheduler::get_current_thread()->cpu_ctx);
+    Log::printk(Log::DEBUG,
+                "Received fast system call %d from PID %d, %lX %lX %lX "
+                "%lX %lX\n",
+                ctx->rax, Scheduler::get_current_thread()->tid, ctx->rdi,
+                ctx->rsi, ctx->rdx, ctx->r10, ctx->r8);
+    if (ctx->rax > 256 || !syscall_table[ctx->rax]) {
+        Log::printk(Log::ERROR, "Received invalid syscall #%d\n", ctx->rax);
+        ctx->rax = -ENOSYS;
+        return;
+    }
+    uint64_t (*func)(uint64_t a, uint64_t b, uint64_t c, uint64_t d,
+                     uint64_t e) =
+        (uint64_t(*)(uint64_t a, uint64_t b, uint64_t c, uint64_t d,
+                     uint64_t e))syscall_table[ctx->rax];
+    ctx->rax = func(ctx->rdi, ctx->rsi, ctx->rdx, ctx->r10, ctx->r8);
+}

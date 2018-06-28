234fb4209e1ece58f8510bdf6648311f3536ef6d
Jason Lu <syscallrax@gmail.com>
2018-06-28 18:39:34 -0500

    fs: ptyfs: Move PTS declaration to header

--- fs/pty/pts.cc
+++ fs/pty/pts.cc
@@ -5,37 +5,6 @@
 
 namespace Filesystem
 {
-class PTSN : public BaseInode
-{
-public:
-    PTSN(ino_t ino, dev_t dev, mode_t mode);
-    virtual ~PTSN();
-    virtual ssize_t pread(uint8_t* buffer, size_t count, off_t offset) override;
-    virtual ssize_t pwrite(uint8_t* buffer, size_t count,
-                           off_t offset) override;
-};
-
-struct PTSNWrapper {
-    PTSNWrapper(Ref<Inode> inode, const char* name);
-    ~PTSNWrapper();
-    Ref<Inode> inode;
-    const char* name;
-    Node<PTSNWrapper> node;
-};
-
-class PTSD : public BaseInode
-{
-public:
-    PTSD(ino_t ino, dev_t dev, mode_t mode);
-    virtual ~PTSD();
-    virtual int link(const char* name, Ref<Inode> node) override;
-    virtual Ref<Inode> open(const char* name, int flags, mode_t mode) override;
-
-private:
-    Ref<Inode> find_child(const char* name);
-    List<PTSNWrapper, &PTSNWrapper::node> children;
-};
-
 PTSN::PTSN(ino_t ino, dev_t dev, mode_t mode)
 {
     this->ino = (ino) ? ino : reinterpret_cast<ino_t>(this);
--- include/fs/pty/pts.h
+++ include/fs/pty/pts.h
@@ -14,4 +14,35 @@ public:
     bool mount(Superblock* sb) override;
     uint32_t flags() override;
 };
+
+class PTSN : public BaseInode
+{
+public:
+    PTSN(ino_t ino, dev_t dev, mode_t mode);
+    virtual ~PTSN();
+    virtual ssize_t pread(uint8_t* buffer, size_t count, off_t offset) override;
+    virtual ssize_t pwrite(uint8_t* buffer, size_t count,
+                           off_t offset) override;
+};
+
+struct PTSNWrapper {
+    PTSNWrapper(Ref<Inode> inode, const char* name);
+    ~PTSNWrapper();
+    Ref<Inode> inode;
+    const char* name;
+    Node<PTSNWrapper> node;
+};
+
+class PTSD : public BaseInode
+{
+public:
+    PTSD(ino_t ino, dev_t dev, mode_t mode);
+    virtual ~PTSD();
+    virtual int link(const char* name, Ref<Inode> node) override;
+    virtual Ref<Inode> open(const char* name, int flags, mode_t mode) override;
+
+private:
+    Ref<Inode> find_child(const char* name);
+    List<PTSNWrapper, &PTSNWrapper::node> children;
+};
 }  // namespace Filesystem

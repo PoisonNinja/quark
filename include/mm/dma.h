8f45d32c2afdac74694faed5cc5a2e3318840ec8
Jason Lu <syscallrax@gmail.com>
2018-08-19 12:21:33 -0500

    mm: dma: Initial implementation of scatter gather lists

--- include/mm/dma.h
+++ include/mm/dma.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <lib/list.h>
 #include <types.h>
 
 namespace Memory
@@ -9,9 +10,29 @@ namespace DMA
 struct Region {
     addr_t virtual_base;
     addr_t physical_base;
-    size_t size;
+    addr_t size;
+    Node<Region> node;
+
+    // Please don't touch this
+    size_t real_size;
+};
+
+struct SGList {
+    size_t num_regions;
+    size_t total_size;
+    List<Region, &Region::node> list;
 };
 
 bool allocate(size_t size, Region& region);
+
+/*
+ * Builds a scatter gather list with either max_elements regions or regions
+ * adding up to total size, whichever comes first
+ *
+ * Callers must be responsible for memory allocated, including clearing the
+ * region memory
+ */
+SGList* build_sglist(size_t max_elements, size_t max_element_size,
+                     size_t total_size);
 } // namespace DMA
 } // namespace Memory
--- mm/dma.cc
+++ mm/dma.cc
@@ -12,6 +12,7 @@ bool allocate(size_t size, Region& region)
     region.virtual_base  = Memory::Valloc::allocate(size);
     region.physical_base = Memory::Physical::allocate(size);
     region.size          = size;
+    region.real_size     = size;
     if (!Memory::Virtual::map_range(region.virtual_base, region.physical_base,
                                     region.size, PAGE_WRITABLE)) {
         Memory::Valloc::free(region.virtual_base);
@@ -19,6 +20,37 @@ bool allocate(size_t size, Region& region)
         return false;
     }
     return true;
+}
+
+SGList* build_sglist(size_t max_elements, size_t max_element_size,
+                     size_t total_size)
+{
+    SGList* sglist   = new SGList();
+    size_t allocated = 0;
+    while (allocated < total_size && sglist->num_regions <= max_elements) {
+        // We always try to allocate the maximum size
+        size_t alloc_size =
+            (max_element_size < total_size) ? max_element_size : total_size;
+        auto alloc            = Memory::Physical::try_allocate(alloc_size);
+        Region* region        = new Region();
+        region->physical_base = alloc.first;
+        region->real_size     = alloc.second;
+        allocated += alloc.second;
+
+        if (allocated < total_size) {
+            region->size = alloc.second;
+        } else {
+            region->size = total_size - (allocated - alloc.second);
+        }
+
+        region->virtual_base = Memory::Valloc::allocate(alloc.second);
+        Memory::Virtual::map_range(region->virtual_base, region->physical_base,
+                                   region->size, PAGE_WRITABLE);
+        sglist->list.push_back(*region);
+        sglist->num_regions++;
+        sglist->total_size += region->size;
+    }
+    return sglist;
 }
 } // namespace DMA
 } // namespace Memory

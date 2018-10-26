cdf6d0f419d10a2c29e4f170fa1a8f0694a58205
Jason Lu <syscallrax@gmail.com>
2018-10-26 16:03:23 -0500

    x86: i686: Give heap and valloc more space to work

--- arch/include/x86/mm/layout.h
+++ arch/include/x86/mm/layout.h
@@ -18,9 +18,9 @@ constexpr addr_t VMA          = 0xFFFFFFFF80000000;
 // 1023 - Fractal mapping
 // 1020 - Copy mapping (for fork)
 
-constexpr addr_t HEAP_START   = 0xFF400000; // Slot 1021
+constexpr addr_t HEAP_START   = 0xE0000000; // Slot 896
 constexpr addr_t STACK_START  = 0xFF800000; // Slot 1022
-constexpr addr_t VALLOC_START = 0xFEC00000; // Slot 1019
+constexpr addr_t VALLOC_START = 0xD0000000; // Slot 832
 constexpr addr_t USER_START   = 0x400000;   // 4 MiB
 constexpr addr_t USER_END     = 0xC0000000; // 3 GB
 constexpr addr_t VMA          = 0xC0000000;

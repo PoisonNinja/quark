609a83ba453a295169b9f1ed399f11189301e3ef
Jason Lu <syscallrax@gmail.com>
2018-11-19 00:08:48 -0600

    lib: utility: Add tuple

--- include/lib/utility.h
+++ include/lib/utility.h
@@ -1,20 +1,9 @@
 #pragma once
 
+#include <lib/type_traits.h>
+
 namespace libcxx
 {
-template <class T>
-struct remove_reference {
-    using type = T;
-};
-template <class T>
-struct remove_reference<T&> {
-    using type = T;
-};
-template <class T>
-struct remove_reference<T&&> {
-    using type = T;
-};
-
 template <class T>
 constexpr typename libcxx::remove_reference<T>::type&& move(T&& t)
 {
@@ -39,6 +28,115 @@ constexpr void swap(T& a, T& b)
     T t = libcxx::move(a);
     a   = libcxx::move(b);
     b   = libcxx::move(t);
+}
+
+template <class... Ts>
+struct tuple;
+
+template <class T, class... Ts>
+struct tuple<T, Ts...> {
+    T first;
+    tuple<Ts...> rest;
+
+    tuple() = default;
+    template <class U, class... Us,
+              class = typename ::libcxx::enable_if<!::libcxx::is_base_of<
+                  tuple, typename ::libcxx::decay<U>::type>::value>::type>
+    tuple(U&& u, Us&&... tail)
+        : first(::libcxx::forward<U>(u))
+        , rest(::libcxx::forward<Us>(tail)...)
+    {
+    }
+};
+
+template <class T>
+struct tuple<T> {
+    T first;
+
+    tuple() = default;
+    template <class U,
+              class = typename ::libcxx::enable_if<!::libcxx::is_base_of<
+                  tuple, typename ::libcxx::decay<U>::type>::value>::type>
+    tuple(U&& u)
+        : first(::libcxx::forward<U>(u))
+    {
+    }
+};
+
+template <>
+struct tuple<> {
+};
+
+namespace detail
+{
+
+template <::size_t i, class T>
+struct tuple_element;
+
+template <::size_t i, class T, class... Ts>
+struct tuple_element<i, tuple<T, Ts...>> : tuple_element<i - 1, tuple<Ts...>> {
+};
+
+template <class T, class... Ts>
+struct tuple_element<0, tuple<T, Ts...>> {
+    using type = T;
+};
+
+template <::size_t i>
+struct tuple_accessor {
+    template <class... Ts>
+    static inline typename tuple_element<i, tuple<Ts...>>::type&
+    get(tuple<Ts...>& t)
+    {
+        return tuple_accessor<i - 1>::get(t.rest);
+    }
+
+    template <class... Ts>
+    static inline const typename tuple_element<i, tuple<Ts...>>::type&
+    get(const tuple<Ts...>& t)
+    {
+        return tuple_accessor<i - 1>::get(t.rest);
+    }
+};
+
+template <>
+struct tuple_accessor<0> {
+    template <class... Ts>
+    static inline typename tuple_element<0, tuple<Ts...>>::type&
+    get(tuple<Ts...>& t)
+    {
+        return t.first;
+    }
+
+    template <class... Ts>
+    static inline const typename tuple_element<0, tuple<Ts...>>::type&
+    get(const tuple<Ts...>& t)
+    {
+        return t.first;
+    }
+};
+
+} // namespace detail
+
+template <class... Ts>
+inline tuple<typename ::libcxx::decay<Ts>::type...> make_tuple(Ts&&... x)
+{
+    return tuple<typename ::libcxx::decay<Ts>::type...>(
+        ::libcxx::forward<Ts>(x)...);
+}
+
+template <::size_t i, class... Ts>
+inline typename detail::tuple_element<i, tuple<Ts...>>::type&
+get(tuple<Ts...>& t)
+{
+    return detail::tuple_accessor<i>::get(t);
+}
+
+template <::size_t i, class... Ts>
+inline const typename detail::tuple_element<i, tuple<Ts...>>::type&
+get(const tuple<Ts...>& t)
+{
+    return detail::tuple_accessor<i>::get(t);
 }
 
 template <typename M, typename N>

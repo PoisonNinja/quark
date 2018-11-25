b3a84139200ab7edd78a71a2d9a31218537cef28
Jason Lu <syscallrax@gmail.com>
2018-11-24 21:56:41 -0600

    lib: type_traits: Add more support functions

--- include/lib/type_traits.h
+++ include/lib/type_traits.h
@@ -578,4 +578,124 @@ struct is_base_of
 
 template <class Base, class Derived>
 inline constexpr bool is_base_of_v = is_base_of<Base, Derived>::value;
+
+template <size_t Len, size_t Align>
+struct aligned_storage {
+    struct type {
+        alignas(Align) unsigned char data[Len];
+    };
+};
+
+template <size_t Len, size_t Align>
+using aligned_storage_t = typename aligned_storage<Len, Align>::type;
+
+template <class T>
+struct alignment_of : libcxx::integral_constant<size_t, alignof(T)> {
+};
+
+template <class T>
+inline constexpr size_t alignment_of_v = alignment_of<T>::value;
+
+namespace __is_convertible_imp
+{
+template <class _Tp>
+void __test_convert(_Tp);
+
+template <class _From, class _To, class = void>
+struct __is_convertible_test : public false_type {
+};
+
+template <class _From, class _To>
+struct __is_convertible_test<
+    _From, _To,
+    decltype(libcxx::__is_convertible_imp::__test_convert<_To>(
+        libcxx::declval<_From>()))> : public true_type {
+};
+
+template <class _Tp, bool _IsArray = is_array<_Tp>::value,
+          bool _IsFunction = is_function<_Tp>::value,
+          bool _IsVoid     = is_void<_Tp>::value>
+struct __is_array_function_or_void {
+    enum { value = 0 };
+};
+template <class _Tp>
+struct __is_array_function_or_void<_Tp, true, false, false> {
+    enum { value = 1 };
+};
+template <class _Tp>
+struct __is_array_function_or_void<_Tp, false, true, false> {
+    enum { value = 2 };
+};
+template <class _Tp>
+struct __is_array_function_or_void<_Tp, false, false, true> {
+    enum { value = 3 };
+};
+} // namespace __is_convertible_imp
+
+template <class _Tp,
+          unsigned = __is_convertible_imp::__is_array_function_or_void<
+              typename remove_reference<_Tp>::type>::value>
+struct __is_convertible_check {
+    static const size_t __v = 0;
+};
+
+template <class _Tp>
+struct __is_convertible_check<_Tp, 0> {
+    static const size_t __v = sizeof(_Tp);
+};
+
+template <class _T1, class _T2,
+          unsigned _T1_is_array_function_or_void =
+              __is_convertible_imp::__is_array_function_or_void<_T1>::value,
+          unsigned _T2_is_array_function_or_void =
+              __is_convertible_imp::__is_array_function_or_void<_T2>::value>
+struct __is_convertible
+    : public integral_constant<
+          bool,
+          __is_convertible_imp::__is_convertible_test<_T1, _T2>::value
+#if defined(_LIBCPP_HAS_NO_RVALUE_REFERENCES)
+              &&
+              !(!is_function<_T1>::value && !is_reference<_T1>::value &&
+                is_reference<_T2>::value &&
+                (!is_const<typename remove_reference<_T2>::type>::value ||
+                 is_volatile<typename remove_reference<_T2>::type>::value) &&
+                (is_same<typename remove_cv<_T1>::type,
+                         typename remove_cv<typename remove_reference<
+                             _T2>::type>::type>::value ||
+                 is_base_of<typename remove_reference<_T2>::type, _T1>::value))
+#endif
+          >{};
+
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 0, 1> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 1, 1> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 2, 1> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 3, 1> : public false_type{};
+
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 0, 2> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 1, 2> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 2, 2> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 3, 2> : public false_type{};
+
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 0, 3> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 1, 3> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 2, 3> : public false_type{};
+template <class _T1, class _T2>
+struct __is_convertible<_T1, _T2, 3, 3> : public true_type{};
+
+template <class _T1, class _T2>
+struct is_convertible : public __is_convertible<_T1, _T2> {
+    static const size_t __complete_check1 = __is_convertible_check<_T1>::__v;
+    static const size_t __complete_check2 = __is_convertible_check<_T2>::__v;
+};
 } // namespace libcxx

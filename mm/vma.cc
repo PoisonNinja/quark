29b686300f4b575f0e45809f6cef11caae7d171b
Jason Lu <syscallrax@gmail.com>
2018-12-22 15:29:04 -0600

    Revert "mm: vma: Make vmregion a private member class of vma"
    Actually never mind, this might be useful
    
    This reverts commit 5c7c07bde8408cf38e9b4513e13b90f62b572009.

--- mm/vma.cc
+++ mm/vma.cc
@@ -6,49 +6,49 @@
 
 namespace Memory
 {
-vma::vmregion::vmregion(addr_t start, size_t size)
+vmregion::vmregion(addr_t start, size_t size)
     : largest_subgap(0)
     , _start(start)
     , _size(size)
 {
 }
 
-vma::vmregion::vmregion(vmregion& other)
+vmregion::vmregion(vmregion& other)
     : vmregion(other._start, other._size)
 {
 }
 
-addr_t vma::vmregion::start() const
+addr_t vmregion::start() const
 {
     return _start;
 }
 
-addr_t vma::vma::vmregion::end() const
+addr_t vmregion::end() const
 {
     return _start + _size;
 }
 
-size_t vma::vmregion::size() const
+size_t vmregion::size() const
 {
     return _size;
 }
 
-bool vma::vmregion::operator==(const vmregion& b) const
+bool vmregion::operator==(const vmregion& b) const
 {
     return (_start == b._start) && (_size == b._size);
 }
 
-bool vma::vmregion::operator!=(const vmregion& b) const
+bool vmregion::operator!=(const vmregion& b) const
 {
     return !(*this == b);
 }
 
-bool vma::vmregion::operator<(const vmregion& b) const
+bool vmregion::operator<(const vmregion& b) const
 {
     return this->_start < b._start;
 }
 
-bool vma::vmregion::operator>(const vmregion& b) const
+bool vmregion::operator>(const vmregion& b) const
 {
     return this->_start > b._start;
 }
@@ -156,7 +156,7 @@ void vma::free(addr_t addr, size_t size)
     delete node;
 }
 
-const vma::vmregion* vma::find(addr_t addr)
+const vmregion* vma::find(addr_t addr)
 {
     const vmregion* curr = sections.get_root();
     const vmregion* ret  = nullptr;

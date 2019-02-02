1fbe718ecba36db42ec4af11dd75c8d3c6194a8d
Jason Lu <syscallrax@gmail.com>
2019-02-02 00:13:39 -0600

    fs: ext2: file: Copy only what's needed

--- fs/ext2/file.cc
+++ fs/ext2/file.cc
@@ -24,11 +24,12 @@ ssize_t ext2_file::read(uint8_t* buffer, size_t count, off_t offset,
     size_t current_block = start_block;
     size_t processed     = 0;
     size_t current       = offset;
+    size_t remaining     = count;
 
     uint8_t tmp[this->instance->geometry.block_size];
 
     while (current_block <= end_block) {
-        this->instance->read_block(tmp, current_block);
+        this->read_data_block(current_block, tmp);
         if (current >
             libcxx::round_down(current, this->instance->geometry.block_size)) {
             size_t in_block_offset =
@@ -39,16 +40,21 @@ ssize_t ext2_file::read(uint8_t* buffer, size_t count, off_t offset,
                                in_block_offset);
             processed += this->instance->geometry.block_size - in_block_offset;
             current += this->instance->geometry.block_size - in_block_offset;
+            remaining -= this->instance->geometry.block_size - in_block_offset;
         } else {
-            libcxx::memcpy(buffer + processed, tmp,
-                           this->instance->geometry.block_size);
+            size_t to_copy = this->instance->geometry.block_size;
+            if (remaining < this->instance->geometry.block_size) {
+                to_copy = remaining;
+            }
+            libcxx::memcpy(buffer + processed, tmp, to_copy);
             processed += this->instance->geometry.block_size;
             current += this->instance->geometry.block_size;
+            remaining -= to_copy;
         }
         current_block++;
     }
 
     return processed;
-}
+} // namespace ext2
 } // namespace ext2
 } // namespace filesystem

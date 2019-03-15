11e01d834086087e24545f8367a2420dcfbdc0d8
Jason Lu <syscallrax@gmail.com>
2019-03-14 23:53:33 -0500

    mm: physical: Switch to stack allocator
    Buddy allocator was broken because it didn't free pages properly when
    coalescing, leading to significant memory leaks.
    
    To fix this, we would need to somehow free an arbitrary page from a
    stack which is virtually impossible without resorting to a linked
    list.
    
    To implement a linked list of pages, we would need to a) map the entire
    physical address space into memory which is undesirable for several
    reasons or b) change the mapping of a specific page to access each
    physical page (similar to how Linux implements high memory).
    
    I tested b), which had significant performance penalties as expected
    for the numerous TLB flushes.

--- arch/x86/kernel/bootstrap32.asm
+++ arch/x86/kernel/bootstrap32.asm
@@ -60,7 +60,8 @@ gdt64:                           ; Global Descriptor Table (64-bit).
 align 4096
 pml4:
     dq (pml3 + 0x7)              ; Identity map
-    times 509 dq 0
+    times 508 dq 0
+    dq (phys_pml3 + 0x7)         ; Stack allocator
     dq (pml4 + 0x7)              ; Fractal mapping
     dq (pml3 + 0x7)              ; -2GB mapping
 
@@ -91,6 +92,26 @@ pml1:
     dq (i << 12) | 0x083
     %assign i i+1
     %endrep
+
+align 4096
+phys_pml3:
+    dq (phys_pml2 + 0x7)
+    times 511 dq 0
+
+align 4096
+phys_pml2:
+    dq (phys_pml1 + 0x7)
+    times 511 dq 0
+
+align 4096
+phys_pml1:
+    dq (phys_pml0 + 0x7)
+    times 511 dq 0
+
+align 4096
+phys_pml0:
+    times 512 dq 0
+
 %else
 
 align 4096

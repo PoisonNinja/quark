a733df8d1deba971c80f4026320792513dc297e2
Jason Lu <syscallrax@gmail.com>
2019-04-13 18:48:17 -0500

    fs: tty: Initial new TTY implementation
    Just boilerplate stuff for now

--- fs/tty.cc
+++ fs/tty.cc
@@ -3,11 +3,13 @@
 #include <fs/stat.h>
 #include <fs/tty.h>
 #include <kernel.h>
+#include <lib/murmur.h>
+#include <lib/unordered_map.h>
 
 namespace
 {
 dev_t tty_major = 0;
-}
+} // namespace
 
 namespace filesystem
 {
@@ -84,6 +86,75 @@ bool register_tty(dev_t major, tty* driver)
 {
     tty_device* tty = new tty_device(driver);
     register_kdevice(CHR, (major) ? major : tty_major, tty);
+    return true;
+}
+
+tty_driver::tty_driver()
+{
+}
+
+ssize_t tty_driver::write(const uint8_t* buffer, size_t count, void* cookie)
+{
+    // Act as a sink
+    return count;
+}
+
+void tty_driver::set_core(tty_core* core)
+{
+    if (core) {
+        log::printk(log::log_level::WARNING,
+                    "tty: Uhh, you already have a core driver registered, this "
+                    "is probably not what you want\n");
+    }
+    this->core = core;
+}
+
+tty_core::tty_core(tty_driver* driver)
+    : kdevice(CHR)
+    , driver(driver)
+{
+}
+
+int tty_core::ioctl(unsigned long request, char* argp, void* cookie)
+{
+    // return this->t->ioctl(request, argp, cookie);
+}
+
+libcxx::pair<int, void*> tty_core::open(const char* name)
+{
+    // return this->t->open(name);
+}
+
+int tty_core::poll(filesystem::poll_register_func_t& callback, void* cookie)
+{
+    // return this->t->poll(callback, cookie);
+}
+
+ssize_t tty_core::read(uint8_t* buffer, size_t count, off_t /* offset */,
+                       void* cookie)
+{
+    // return this->t->read(buffer, count, cookie);
+}
+
+ssize_t tty_core::write(const uint8_t* buffer, size_t count, off_t /* offset */,
+                        void* cookie)
+{
+    // return this->t->write(buffer, count, cookie);
+}
+
+bool register_tty_ng(tty_driver* driver, dev_t major, dev_t minor)
+{
+    if (!driver) {
+        return false;
+    }
+    if (!major) {
+        major = tty_major;
+    } else {
+        register_class(filesystem::CHR, major);
+    }
+    tty_core* tty = new tty_core(driver);
+    driver->set_core(tty);
+    register_kdevice(filesystem::CHR, major, tty);
     return true;
 }
 
--- include/fs/tty.h
+++ include/fs/tty.h
@@ -40,7 +40,56 @@ private:
     tty* t;
 };
 
+class tty_core;
+
+typedef unsigned char cc_t;
+typedef unsigned int speed_t;
+typedef unsigned int tcflag_t;
+
+/*
+ * To facilitate ptmx, per-terminal data is stored in the tty_driver
+ * itself.
+ */
+
+struct termios {
+};
+
+class tty_driver
+{
+public:
+    tty_driver();
+    virtual ssize_t write(const uint8_t* buffer, size_t count, void* cookie);
+    void set_core(tty_core* core);
+    struct termios* get_termios();
+
+private:
+    tty_core* core;
+};
+
+class tty_core : public kdevice
+{
+public:
+    tty_core(tty_driver* driver);
+    int ioctl(unsigned long request, char* argp, void* cookie) override final;
+    libcxx::pair<int, void*> open(const char* name) override;
+    int poll(filesystem::poll_register_func_t& callback, void* cookie) override;
+    ssize_t read(uint8_t* buffer, size_t count, off_t offset,
+                 void* cookie) override;
+    ssize_t write(const uint8_t* buffer, size_t count, off_t offset,
+                  void* cookie) override;
+
+public:
+    ssize_t notify(const uint8_t* buffer, size_t count);
+
+private:
+    tty_driver* driver;
+
+    char* buffer;
+};
+
 bool register_tty(dev_t major, tty* tty);
+
+bool register_tty_ng(tty_driver* driver, dev_t major, dev_t minor);
 
 void init();
 } // namespace tty

eca9f319b608d1b91c67595896e5472fb377e3c3
Jason Lu <syscallrax@gmail.com>
2019-05-16 22:30:32 -0500

    x86_64: Adjust build files

--- arch/x86_64/module.cmake
+++ arch/x86_64/module.cmake
@@ -1,24 +1,10 @@
-if (ARCH STREQUAL "x86_64")
-    add_definitions(
-        -DX86_64
-    )
-endif()
+add_definitions(
+    -DX86_64
+)
 
-if (ARCH STREQUAL "x86_64")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcmodel=large -mno-red-zone")
-else()
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-endif()
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -zmax-page-size=0x1000 -mcmodel=large -mno-red-zone")
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -Wl,-r,-T ${ARCH_PATH}/module.lds")
-if (ARCH STREQUAL "x86_64")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -zmax-page-size=0x1000")
-endif()
 
-set(CMAKE_ASM_NASM_FLAGS "${CMAKE_ASM_NASM_FLAGS} -F dwarf")
-if (ARCH STREQUAL "x86_64")
-    set(CMAKE_ASM_NASM_FLAGS "${CMAKE_ASM_NASM_FLAGS} -d X86_64 -i${CMAKE_SOURCE_DIR}/arch/include/x86/asm/")
-    set(CMAKE_ASM_NASM_OBJECT_FORMAT elf64)
-else()
-    set(CMAKE_ASM_NASM_OBJECT_FORMAT elf)
-endif()
+set(CMAKE_ASM_NASM_FLAGS "${CMAKE_ASM_NASM_FLAGS} -F dwarf -d X86_64 -i${CMAKE_SOURCE_DIR}/arch/include/x86/asm/")
+set(CMAKE_ASM_NASM_OBJECT_FORMAT elf64)
--- arch/x86_64/platform.cmake
+++ arch/x86_64/platform.cmake
@@ -1,28 +1,10 @@
 add_definitions(
-    -DX86
+    -DX86_64
 )
 
-if (ARCH STREQUAL "x86_64")
-    add_definitions(
-        -DX86_64
-    )
-endif()
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcmodel=kernel -mno-red-zone -zmax-page-size=0x1000")
 
-if (ARCH STREQUAL "x86_64")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcmodel=kernel -mno-red-zone")
-else()
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-endif()
-
-if (ARCH STREQUAL "x86_64")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -zmax-page-size=0x1000")
-endif()
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T${CMAKE_BINARY_DIR}/quark.lds")
 
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -F dwarf")
-if (ARCH STREQUAL "x86_64")
-    set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -d X86_64 -i${CMAKE_SOURCE_DIR}/arch/include/x86/asm/")
-    set(CMAKE_ASM_NASM_OBJECT_FORMAT elf64)
-else()
-    set(CMAKE_ASM_NASM_OBJECT_FORMAT elf)
-endif()
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -F dwarf -d X86_64 -i${CMAKE_SOURCE_DIR}/arch/include/x86_64/asm/")
+set(CMAKE_ASM_NASM_OBJECT_FORMAT elf64)

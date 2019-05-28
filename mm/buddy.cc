d9cae9b6effaf7623953cd8d70d62f7bf25baed5
Jason Lu <syscallrax@gmail.com>
2019-05-27 20:02:08 -0500

    mm: buddy: Fix coalescing behavior

--- mm/buddy.cc
+++ mm/buddy.cc
@@ -25,16 +25,12 @@ struct buddy_order {
 buddy::buddy(size_t s, size_t min, size_t max)
 {
     // TODO: Perform sanity checks
-    this->size          = s;
-    this->min_order     = min;
-    this->max_order     = max;
-    this->orders        = new buddy_order[max + 1];
-    size_t stack_offset = 0;
+    this->size      = s;
+    this->min_order = min;
+    this->max_order = max;
+    this->orders    = new buddy_order[max + 1];
     for (size_t i = min; i <= max; i++) {
-        this->orders[i].free_stack =
-            new stack(reinterpret_cast<addr_t*>(STACK_START + stack_offset));
-        stack_offset +=
-            memory::virt::align_up(stack_overhead(this->size, libcxx::pow2(i)));
+        this->orders[i].free_stack = new stack();
         this->orders[i].bitset =
             new libcxx::bitset(this->size / libcxx::pow2(i), true);
     }
@@ -50,45 +46,32 @@ addr_t buddy::alloc(size_t size)
     if (order < this->min_order)
         order = this->min_order;
     if (order > this->max_order)
-        return 0;
-    size_t original_order = order;
+        kernel::panic("OOM");
     if (this->orders[order].free_stack->empty()) {
-        bool found = false;
-        while (order++ <= this->max_order) {
-            if (!this->orders[order].free_stack->empty()) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            return 0;
-        }
-        addr_t addr = this->orders[order].free_stack->pop();
-        for (; order > original_order; order--) {
-            this->orders[order].bitset->set(buddy_index(addr, order));
-            this->orders[order - 1].bitset->set(buddy_index(addr, order - 1));
-            this->orders[order - 1].free_stack->push(
-                buddy_address(addr, order - 1));
-        }
-        return addr;
-    } else {
-        addr_t addr = this->orders[order].free_stack->pop();
-        this->orders[order].bitset->set(buddy_index(addr, order));
-        return addr;
+        addr_t a = this->alloc(libcxx::pow2(order + 1));
+        this->orders[order].free_stack->push(a);
+        this->orders[order].free_stack->push(a + libcxx::pow2(order));
     }
+    addr_t addr = this->orders[order].free_stack->pop();
+    this->orders[order].bitset->set(buddy_index(addr, order));
+    return addr;
 }
 
 void buddy::free(addr_t addr, size_t size)
 {
     uint32_t order = libcxx::log2(size);
-    for (; order <= this->max_order; order++) {
-        this->orders[order].bitset->unset(buddy_index(addr, order));
-        addr_t buddy_addr = buddy_address(addr, order);
-        if (this->orders[order].bitset->test(buddy_index(buddy_addr, order)) ||
-            order == this->max_order) {
-            this->orders[order].free_stack->push(addr);
-            break;
-        }
+    if (order > this->max_order) {
+        return;
+    }
+    this->orders[order].bitset->unset(buddy_index(addr, order));
+    addr_t buddy_addr = buddy_address(addr, order);
+    if (!this->orders[order].bitset->test(buddy_index(buddy_addr, order))) {
+        this->orders[order].free_stack->remove(buddy_addr);
+        this->free((addr < buddy_addr) ? addr : buddy_addr,
+                   libcxx::pow2(order + 1));
+    } else {
+        this->orders[order].free_stack->push(addr);
+        return;
     }
 }
 

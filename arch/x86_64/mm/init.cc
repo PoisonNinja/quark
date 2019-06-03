9957a75a1a3bda4e30d013116a02ee42c473f4d6
Jason Lu <syscallrax@gmail.com>
2019-06-03 21:49:20 +0800

    x86_64: mm: Remove direct RAM map
    Instead of 512GB, in theory x86_64 supports a theoretical maximum of
    512 GB / sizeof(struct page) * 4 KiB = 128 TB of RAM now.
    
    Of course, if struct page increases in size this will go down, so
    priority is on keeping struct page as minimal as possible.
    
    Another option is giving pagedb access to more RAM. For example, giving
    it two PML4 slots would give it 1 TB which would double the amount of
    RAM that Quark supports.

--- arch/x86_64/mm/init.cc
+++ arch/x86_64/mm/init.cc
@@ -12,76 +12,8 @@
 
 namespace memory
 {
-namespace
-{
-void init_phys_map()
-{
-    bool supports_1gb = false;
-    if (cpu::x86_64::has_feature(*cpu::get_current_core(),
-                                 X86_FEATURE_GBPAGES)) {
-        supports_1gb = true;
-        log::printk(log::log_level::INFO,
-                    "CPU supports 1 GB pages, using it\n");
-    } else {
-        log::printk(
-            log::log_level::WARNING,
-            "CPU does not support 1 GB pages, falling back to 2MB pages\n");
-    }
-    struct memory::virt::page_table *top_level =
-        (struct memory::virt::page_table *)memory::x86_64::decode_fractal(
-            memory::x86_64::recursive_entry, memory::x86_64::recursive_entry,
-            memory::x86_64::recursive_entry, memory::x86_64::recursive_entry);
-    struct memory::virt::page_table *second_level =
-        static_cast<struct memory::virt::page_table *>(
-            memory::x86_64::decode_fractal(
-                memory::x86_64::recursive_entry,
-                memory::x86_64::recursive_entry,
-                memory::x86_64::recursive_entry,
-                memory::x86_64::pml4_index(PHYS_START)));
-    top_level->pages[memory::x86_64::pml4_index(PHYS_START)].present  = 1;
-    top_level->pages[memory::x86_64::pml4_index(PHYS_START)].writable = 1;
-    top_level->pages[memory::x86_64::pml4_index(PHYS_START)].address =
-        memory::physical::allocate() / 0x1000;
-    memory::x86_64::invlpg(reinterpret_cast<addr_t>(second_level));
-    libcxx::memset(second_level, 0, sizeof(*second_level));
-    for (int i = 0; i < 512; i++) {
-        second_level->pages[i].present  = 1;
-        second_level->pages[i].nx       = 1;
-        second_level->pages[i].writable = 1;
-        if (supports_1gb) {
-            second_level->pages[i].huge_page = 1;
-            second_level->pages[i].address   = i * 0x40000;
-        } else {
-            /*
-             * TODO: Tune this based on how much actual physical memory is
-             * present. It's probably overkill to map 256 TB if only 128 MB
-             * is present
-             */
-            struct memory::virt::page_table *third_level =
-                static_cast<struct memory::virt::page_table *>(
-                    memory::x86_64::decode_fractal(
-                        memory::x86_64::recursive_entry,
-                        memory::x86_64::recursive_entry,
-                        memory::x86_64::pml4_index(PHYS_START), i));
-            second_level->pages[i].address =
-                memory::physical::allocate() / 0x1000;
-            memory::x86_64::invlpg(reinterpret_cast<addr_t>(second_level));
-            libcxx::memset(third_level, 0, sizeof(*third_level));
-            for (int j = 0; j < 512; j++) {
-                third_level->pages[j].present   = 1;
-                third_level->pages[j].nx        = 1;
-                third_level->pages[j].writable  = 1;
-                third_level->pages[j].huge_page = 1;
-                third_level->pages[j].address   = (0x40000 * i) + (0x200 * j);
-            }
-        }
-    }
-}
-} // namespace
-
 void arch_init(struct boot::info &info)
 {
-    init_phys_map();
     struct multiboot_fixed *multiboot =
         reinterpret_cast<struct multiboot_fixed *>(info.architecture_data);
     addr_t multiboot_start =

24fa13241c6090732fde53c43c6c89fc5ffd0ea2
Jason Lu <syscallrax@gmail.com>
2019-10-20 00:21:18 -0400

    drivers: input: intel-8042: Rename class to comply with coding style

--- drivers/input/intel-8042.cc
+++ drivers/input/intel-8042.cc
@@ -12,10 +12,10 @@ namespace
 {
 constexpr size_t buffer_size = 1024;
 
-class Intel8042 : public filesystem::kdevice
+class intel8042 : public filesystem::kdevice
 {
 public:
-    Intel8042();
+    intel8042();
 
     virtual int poll(filesystem::poll_register_func_t& callback,
                      void* cookie) override;
@@ -33,13 +33,13 @@ private:
     scheduler::wait_queue queue;
 };
 
-Intel8042::Intel8042()
+intel8042::intel8042()
     : kdevice(filesystem::CHR)
     , head(0)
     , tail(0)
 {
     interrupt::handler* h = new interrupt::handler(
-        libcxx::bind(&Intel8042::handler, this, _1, _2, _3), "keyboard",
+        libcxx::bind(&intel8042::handler, this, _1, _2, _3), "keyboard",
         reinterpret_cast<void*>(this));
     interrupt::register_handler(interrupt::irq_to_interrupt(1), *h);
     /*
@@ -49,7 +49,7 @@ Intel8042::Intel8042()
     inb(0x60);
 }
 
-int Intel8042::poll(filesystem::poll_register_func_t& callback, void* cookie)
+int intel8042::poll(filesystem::poll_register_func_t& callback, void* cookie)
 {
     /*
      * Register this thread with the queue
@@ -61,7 +61,7 @@ int Intel8042::poll(filesystem::poll_register_func_t& callback, void* cookie)
     return 0;
 }
 
-ssize_t Intel8042::read(uint8_t* buffer, size_t count, off_t /*offset*/, void*)
+ssize_t intel8042::read(uint8_t* buffer, size_t count, off_t /*offset*/, void*)
 {
     size_t read = 0;
     while (read < count) {
@@ -73,18 +73,18 @@ ssize_t Intel8042::read(uint8_t* buffer, size_t count, off_t /*offset*/, void*)
     return read;
 }
 
-ssize_t Intel8042::write(const uint8_t*, size_t, off_t, void*)
+ssize_t intel8042::write(const uint8_t*, size_t, off_t, void*)
 {
     return 0;
 }
 
-void Intel8042::handler(int, void* dev_id, struct interrupt_context*)
+void intel8042::handler(int, void* dev_id, struct interrupt_context*)
 {
     this->buffer[this->head++ % buffer_size] = inb(0x60);
     this->queue.wakeup();
 }
 
-bool Intel8042::seekable()
+bool intel8042::seekable()
 {
     return false;
 }
@@ -96,7 +96,7 @@ int init()
 {
     dev_t major = filesystem::locate_class(filesystem::CHR);
     filesystem::register_class(filesystem::CHR, major);
-    Intel8042* d = new Intel8042();
+    intel8042* d = new intel8042();
     filesystem::register_kdevice(filesystem::CHR, major, d);
     return 0;
 }

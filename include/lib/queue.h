326f508cbd78a037e0707a7900162e29df4fd7ff
Jason Lu <jason@jasonxlu.com>
2020-03-13 21:46:51 -0500

    lib: Add queue
    An adapter for list

---
+++ include/lib/queue.h
@@ -0,0 +1,55 @@
+#include <lib/list.h>
+
+namespace libcxx
+{
+template <typename T, libcxx::node<T> T::*Link,
+          class Container = libcxx::list<T, Link>>
+class queue
+{
+private:
+    Container container;
+
+public:
+    queue(){};
+    ~queue(){};
+
+    queue(const queue& other) = delete;
+    queue& operator=(const queue& other) = delete;
+
+    bool empty()
+    {
+        return this->container.empty();
+    }
+
+    size_t size()
+    {
+        return this->container.size();
+    }
+
+    T& back()
+    {
+        return this->container.back();
+    }
+
+    T& front()
+    {
+        return this->container.front();
+    }
+
+    void push(T& node)
+    {
+        this->container.push_back(node);
+    }
+
+    void pop()
+    {
+        this->container.pop_front();
+    }
+
+    // Special for Quark
+    void erase(T& node)
+    {
+        this->container.erase(this->container.iterator_to(node));
+    }
+};
+} // namespace libcxx

960c0500061aaf94f043c65f3001f29eed0a228d
Jason Lu <jason@jasonxlu.com>
2020-03-17 18:43:50 -0500

    kernel: lock: Add spinlock variant with IRQs disabled
    Needed for functions that can be called from interrupts

--- include/kernel/lock.h
+++ include/kernel/lock.h
@@ -8,13 +8,19 @@ class spinlock
 {
 public:
     spinlock();
+
     void lock();
+    int lock_irq();
+
     void unlock();
+    void unlock_irq(int save);
 
     spinlock(const spinlock&) = delete;
     spinlock& operator=(const spinlock&) = delete;
 
 private:
+    void _lock();
+    void _unlock();
     std::atomic_flag locked;
 };
 
@@ -54,5 +60,30 @@ public:
     }
 
 private:
+    T& lock;
+};
+
+template <class T, class Enable = void>
+class scoped_lock_irq;
+
+template <class T>
+class scoped_lock_irq<
+    T, typename std::enable_if<std::conjunction_v<
+           std::is_member_function_pointer<decltype(&T::lock_irq)>,
+           std::is_member_function_pointer<decltype(&T::unlock_irq)>>>::type>
+{
+public:
+    scoped_lock_irq(T& mutex)
+        : lock(mutex)
+    {
+        flag = lock.lock_irq();
+    };
+    ~scoped_lock_irq()
+    {
+        lock.unlock_irq(flag);
+    }
+
+private:
+    int flag;
     T& lock;
 };
--- kernel/spinlock.cc
+++ kernel/spinlock.cc
@@ -1,4 +1,5 @@
 #include <cpu/cpu.h>
+#include <cpu/interrupt.h>
 #include <kernel/lock.h>
 
 spinlock::spinlock()
@@ -6,16 +7,44 @@ spinlock::spinlock()
 {
 }
 
-void spinlock::lock()
+void spinlock::_lock()
 {
     // TODO: Skip this step on UP configurations
     // For now we have no way to tell, so keep this in.
     while (locked.test_and_set(std::memory_order_acquire))
         cpu::halt();
+}
+
+void spinlock::lock()
+{
     // TODO: We also need to disable kernel preemption properly
+    this->_lock();
+}
+
+int spinlock::lock_irq()
+{
+    int ret = interrupt::save();
+    interrupt::disable();
+
+    this->_lock();
+
+    return ret;
+}
+
+void spinlock::_unlock()
+{
+    locked.clear(std::memory_order_release);
 }
 
 void spinlock::unlock()
 {
-    locked.clear(std::memory_order_release);
+    // TODO: We also need to reenable kernel preemption properly
+    this->_unlock();
+}
+
+void spinlock::unlock_irq(int save)
+{
+    this->_unlock();
+
+    interrupt::restore(save);
 }

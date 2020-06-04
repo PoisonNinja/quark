24b51caca7170e81b69689e7379313a1eee02fe8
Jason Lu <jason@jasonxlu.com>
2020-06-03 23:14:44 -0500

    kernel: mutex: Use if instead of while

--- kernel/mutex.cc
+++ kernel/mutex.cc
@@ -8,7 +8,7 @@ mutex::mutex()
 void mutex::lock()
 {
     // For now we have no way to tell, so keep this in.
-    while (locked.test_and_set(std::memory_order_acquire)) {
+    if (locked.test_and_set(std::memory_order_acquire)) {
         // We'd rather not be woken up by a signal
         queue.wait(0, [&]() {
             return !locked.test_and_set(std::memory_order_acquire);
